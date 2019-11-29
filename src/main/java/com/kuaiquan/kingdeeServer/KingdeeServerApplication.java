392ca17dcbb6112df59f8562766d7de0411c43b3
xxxxzb <benzjf@live.com>
2019-11-29 10:08:44 +0800

    增加了说明

---
+++ src/main/java/com/kuaiquan/kingdeeServer/KingdeeServerApplication.java
@@ -0,0 +1,13 @@
+package com.kuaiquan.kingdeeServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KingdeeServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KingdeeServerApplication.class, args);
+	}
+
+}
