ad91fe0eaf18341ff14e66f93dc83b621d5f91f7
xxxx1.0 <327481003@qq.com>
2019-12-07 09:51:18 +0800

    Update吊毛操作1

--- src/main/java/com/kuaiquan/kingdeeServer/KingdeeServerApplication.java
+++ src/main/java/com/kuaiquan/kingdeeServer/KingdeeServerApplication.java
@@ -7,7 +7,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class KingdeeServerApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(KingdeeServerApplication.class, args);
+
+		System.out.println("11");
+
 	}
 
 }
